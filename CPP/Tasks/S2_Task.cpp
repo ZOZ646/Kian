3a86850bf9cf74524f553bc0b06658409b8cb0ce
Mazen_Elsayed <elzoz2160@gmail.com>
2026-07-05 16:04:02 +0300

    Organised

---
+++ CPP/Tasks/S2_Task.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // INFO
+
+    string name;
+    int age;
+    float height;
+
+    cout << "Enter Your Name: ";
+    cin >> name;
+
+    cout << "Enter Your Age: ";
+    cin >> age;
+
+    cout << "Enter Your Height: ";
+    cin >> height;
+
+    cout << "====================== INFO ========================\n";
+
+    cout << "Name: " << name << "\t||\t" << "age: " << age << "\t||\t" << "Height: " << height << endl;
+
+    // Data Types Sizes
+    cout << "======================Data Types Sizes======================\n";
+
+    cout << "- Size Of 'Integer': " << sizeof(int) << " Bytes" << endl;
+    cout << "- Size Of 'Character': " << sizeof(char) << " Byte" << endl;
+    cout << "- Size Of 'string': " << sizeof(string) << " Bytes" << endl;
+    cout << "- Size Of 'float': " << sizeof(float) << " Bytes" << endl;
+    cout << "- Size Of 'double': " << sizeof(double) << " Bytes" << endl;
+
+    return 0;
+
+    // Commit Test
+}
