1e4d26caa4c8da9253ccb74c7d62d9531ad4d8f4
ZOZ646 <162740734+ZOZ646@users.noreply.github.com>
2026-07-07 19:10:51 +0300

    S3_Task

---
+++ CPP/Project_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+    string name;
+    int age;
+    string Phone_Number;
+
+    cout << "Enter Your Name: ";
+    cin >> name;
+
+    cout << "Enter Your Age: ";
+    cin >> age;
+
+    cout << "Enter Your Phone_Number: ";
+    cin >> Phone_Number;
+
+    cout << "name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Phone_Number: " << Phone_Number << endl;
+
+}
---
+++ CPP/arithmatic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x, y, z;
+
+    cout << "Enter 3 Numbers: \n";
+    cin >> x >> y >> z;
+
+    cout << "Sum Of 3 Numbers Is: " << x + y + z << endl;
+    cout << "Average Of The 3 Numbers Is: " << (x + y + z) / 3;
+
+}
