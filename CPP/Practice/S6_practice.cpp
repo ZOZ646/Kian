96bd38d790bbf117c6a9abe485d0f32461655708
ZOZ646 <162740734+ZOZ646@users.noreply.github.com>
2026-07-19 23:16:46 +0300

    Session 6 Practice

---
+++ CPP/Practice/S6_practice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    // int x = 1, grade;
+    // while (x <= 6){
+    //     cout << "Enter our Grade: " << endl;
+    //     cin  >> grade;
+    //     x++;
+    // }
+
+      for (int i = 1; i <= 20 ; i++){
+      if (i % 2 == 0){
+        cout << i << endl;
+      }
+  }
+
+}
