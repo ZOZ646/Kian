c35b3e8e3c1f47a3da31cc7e88ef62eee12f415a
Mazen_Elsayed <elzoz2160@gmail.com>
2026-08-09 00:06:29 +0300

    3akher commit wa3d

--- CPP/Tasks/Guss_The_Number.cpp
+++ CPP/Tasks/Guss_The_Number.cpp
@@ -2,30 +2,51 @@
 #include <time.h>
 using namespace std;
 
+void show();
+int choice ();
+bool play (int max_tries);
+
 int main () {
-    int secret_num;
+show();
+int max_tries = choice();
+bool win = play (max_tries);
+
+while (true) {
+    bool playAgain;
+    cout << "Want to  play again ?" << endl;
+    cout << "1 => Yes\n";
+    cout << "0 => No\n";
+    cin >> playAgain;
+    system ("cls");
+
+    if (playAgain == 1) {
+        max_tries = choice();
+        win = play (max_tries);
+    }
+    else {
+        cout << "Good bye" << endl;
+        break;
+    }
+}
+}
+
+void show() {
+    cout << "\t\t\t\t==============================\n";
+    cout << "\t\t\t\t    GUESS THE NUMBER GAME\n";
+    cout << "\t\t\t\t==============================\n\n";
+}
+
+int choice () {
     int tries = 0;
-    int choice;
     int max_tries;
-    int guessed_num;
-    bool win = false;
-
-    srand(time(0));
-    secret_num = rand() % 100 + 1;
-
-    cout << "==============================\n";
-    cout << "    GUESS THE NUMBER GAME\n";
-    cout << "==============================\n\n";
-
     cout << "Choose a difficulty level:\n";
     cout << "  1 - Easy   \n";
     cout << "  2 - Medium \n";
     cout << "  3 - Hard   \n";
     cout << "\nEnter 1, 2, or 3: ";
+    int choice;
     cin >> choice;
     system ("cls");
-
-
     while (choice != 1 && choice != 2 && choice != 3){ 
     cout << "Choose a difficulty level:\n";
     cout << "  1 - Easy   \n";
@@ -35,9 +56,7 @@ int main () {
     cin >> choice;
     system ("cls");
     }
-
-
-    if (choice == 1){
+        if (choice == 1){
         max_tries = 10;
         cout << "You Have 10 Chances\n" << endl;
     }
@@ -50,7 +69,19 @@ int main () {
         cout << "You Have 5 Chances\n" << endl;
     }
 
-    
+    return max_tries;
+}
+
+bool play (int max_tries) {
+    int tries = 0;
+
+    int secret_num;
+    int guessed_num;
+    bool win = false;
+
+    srand(time(0));
+    secret_num = rand() % 100 + 1;
+
     while (tries < max_tries) {
         int tries_left = max_tries - tries;
         cout << "You Have '" << tries_left << "' tries left\n" << endl;
@@ -75,10 +106,12 @@ int main () {
 
     if (win == true) {
         cout << "Correct ! You got that Within " << tries << " tries of " << max_tries << endl;
+        cout << "The Number Was: " << secret_num << endl;
     }
     else {
         cout << "Hard Luck" << endl;
         cout << "The Number Was: " << secret_num << endl;
     }
 
+    return win;
 }
